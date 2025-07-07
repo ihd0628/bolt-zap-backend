45b8fdaeae88a89ed7deca596ed8ca944e179dc2
seunghoon <si932174@gmail.com>
2025-07-07 21:35:15 +0900

    feat: add Invoices module with controller, service, and unit tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { WalletModule } from './wallet/wallet.module'; // WalletModule 임포트
+import { InvoicesModule } from './invoices/invoices.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { WalletModule } from './wallet/wallet.module'; // WalletModule 임포트
     PrismaModule,
     UsersModule,
     WalletModule,
+    InvoicesModule,
   ],
 })
 export class AppModule {}
